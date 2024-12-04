89cc8c278ee78c92d6f0df1ea60043febae63d93
sgw <rnjsdn2481@naver.com>
2024-12-04 16:41:34 +0900

    dd

--- src/main/java/com/yedam/app/PrjtApplication.java
+++ src/main/java/com/yedam/app/PrjtApplication.java
@@ -1,5 +1,7 @@
 package com.yedam.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 @SpringBootApplication
 public class PrjtApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PrjtApplication.class);
 
 	@Value("${file.uploadpath}")
 	String uploadPath;
@@ -20,6 +25,7 @@ public class PrjtApplication {
 	
 	@GetMapping("/")
 	public String main(Model model) {
+		logger.info("main");
 		model.addAttribute("upload",uploadPath);
 		return "main";
 	}
